64d22b87162b997f98efc8192f1e3b6d3ad2a386
sontek <sontek@yeah.net>
2018-06-05 18:00:10 +0800

    add test

--- src/main/java/com/javaliu/Test.java
+++ src/main/java/com/javaliu/Test.java
@@ -8,4 +8,12 @@ package com.javaliu;
  * <br> 版本：V1.0.0
  */
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(1<<0);
+        System.out.println(1<<2);
+        System.out.println(1<<3);
+        System.out.println(1<<4);
+        System.out.println(4 & 5);
+    }
 }
--- src/main/java/com/javaliu/nio/FileChannelTest.java
+++ src/main/java/com/javaliu/nio/FileChannelTest.java
@@ -2,7 +2,6 @@ package com.javaliu.nio;
 
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
 
 /**
@@ -45,32 +44,51 @@ public class FileChannelTest {
         RandomAccessFile randomAccessFile = new RandomAccessFile("text.txt", "r");
         FileChannel fileChannel = randomAccessFile.getChannel();
         // hello World
-        ByteBuffer buffer = ByteBuffer.allocate(12);
+        ByteBuffer buffer = ByteBuffer.allocate(36);
 
         int len = fileChannel.read(buffer);
         String str = "";
-        int LF = 10;
+        int LF = 10; // 回车
+        int CR = 13; // 换行
         byte[] temp = new byte[0];
+        byte[] lineByte = new byte[0];
         while(-1 != len){
-            buffer.flip();
+            buffer.rewind();
             int size = buffer.remaining();
             byte[] bytes = new byte[size];
-            boolean hasLF = false;
+            buffer.get(bytes);
+            buffer.clear();
 
-            int start = 0;
-            int index = 0;
-            while (buffer.hasRemaining()){
-                index++;
-                byte b = buffer.get();
-                if(b == LF){
+            int lineStartIndex = 0;
+            boolean hasLF = false;
+            for (int i=0;i<size;i++){
+                byte b = buffer.get(i);
+                if(LF == b){
                     hasLF = true;
-                }
-                if(hasLF){
-                    temp = new byte[index-start];
-                    System.arraycopy(bytes, start, temp, 0, temp.length);
+                    int tempSize = temp.length;
+                    int lineNum = i - lineStartIndex;
+                    lineByte = new byte[lineNum + tempSize];
+                    System.arraycopy(temp, 0, lineByte, 0, temp.length);
+                    temp = new byte[0];
+                    System.arraycopy(bytes, lineStartIndex, lineByte, tempSize, lineNum);
+                    System.out.println(new String(lineByte, "UTF-8"));
+                    if(i+1 <size && bytes[i+1] == CR){
+                        lineStartIndex = i + 2;
+                    }else {
+                        lineStartIndex = i + 1;
+                    }
                 }
             }
-            buffer.flip();
+            if(hasLF){
+                temp = new byte[size-lineStartIndex];
+                System.arraycopy(bytes, lineStartIndex, temp, 0, temp.length);
+            }else{
+                byte[] toTemp = new byte[temp.length + bytes.length];
+                System.arraycopy(temp, 0, toTemp, 0, temp.length);
+                System.arraycopy(bytes, 0, toTemp, temp.length, bytes.length);
+                temp = toTemp;
+            }
+            //buffer.flip();
             len = fileChannel.read(buffer);
         }
         fileChannel.close();
@@ -90,6 +108,7 @@ public class FileChannelTest {
             buffer.flip();
             while (buffer.hasRemaining()){
                 System.out.print((char) buffer.get());
+
             }
             System.out.println("");
             //buffer.clear();
