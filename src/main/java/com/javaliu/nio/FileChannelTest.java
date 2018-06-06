a63e6001facc544b3ff29e75e9a7a90c627a9f78
sontek <sontek@yeah.net>
2018-06-06 17:34:01 +0800

    add FileChannelTest.java

--- src/main/java/com/javaliu/nio/FileChannelTest.java
+++ src/main/java/com/javaliu/nio/FileChannelTest.java
@@ -1,5 +1,6 @@
 package com.javaliu.nio;
 
+
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -17,6 +18,12 @@ public class FileChannelTest {
 
         //writeData();
         readData();
+        //System.out.println(new String(new byte[]{49,124,49,50,51,124,-27,-68,-96,-28,-72,-119,13,10}, "UTF-8"));
+/*        System.out.println(new String(new byte[]{50,124,49,50,52,124,-27,-114,-122,-27,-113,-78,10}, "UTF-8"));
+        System.out.println(new String(new byte[]{51,124,49,50,56,124,-25,-114,-117,-28,-70,-108,10}, "UTF-8"));
+        System.out.println(new String(new byte[]{52,124,49,50,53,124,-24,-75,-75,-27,-123,-83,10}, "UTF-8"));
+        System.out.println(new String(new byte[]{53,124,49,50,54,124,-23,-87,-84,-28,-72,-119,10}, "UTF-8"));
+        System.out.println(new String(new byte[]{54,124,49,50,55,124,-26,-75,-117,-24,-81,-107,10}, "UTF-8"));*/
         //truncateFile();
     }
 
@@ -40,30 +47,43 @@ public class FileChannelTest {
     }
 
     // 读取数据
+
+    /**
+     * 读区文件的示例
+     *
+     * 1|123|张三
+     * 2|124|历史
+     * 3|128|王五
+     * 4|125|赵六
+     * 5|126|马三
+     * 6|127|测试
+     *
+     * @throws Exception
+     */
     public static void readData() throws Exception{
         RandomAccessFile randomAccessFile = new RandomAccessFile("text.txt", "r");
         FileChannel fileChannel = randomAccessFile.getChannel();
         // hello World
-        ByteBuffer buffer = ByteBuffer.allocate(36);
+        ByteBuffer buffer = ByteBuffer.allocate(90);
 
         int len = fileChannel.read(buffer);
         String str = "";
-        int LF = 10; // 回车
-        int CR = 13; // 换行
+        int CR = 13; // 回车
+        int LF = 10; // 换行
         byte[] temp = new byte[0];
         byte[] lineByte = new byte[0];
         while(-1 != len){
-            buffer.rewind();
-            int size = buffer.remaining();
+            buffer.flip();                  // 切换模式
+            int size = buffer.remaining();  // 读取到的字节数
             byte[] bytes = new byte[size];
-            buffer.get(bytes);
-            buffer.clear();
+            buffer.get(bytes);              // 将buffer 中的数据转移到 bytes 字节数组中
+            buffer.clear();                 // 清空 buffer，即 position = 0, limit == capacity
 
-            int lineStartIndex = 0;
+            int lineStartIndex = 0;         // 记录每行的首行索引值
             boolean hasLF = false;
             for (int i=0;i<size;i++){
                 byte b = buffer.get(i);
-                if(LF == b){
+                if(LF == b || CR == b){                // 所读取的字节中是否有 换行
                     hasLF = true;
                     int tempSize = temp.length;
                     int lineNum = i - lineStartIndex;
@@ -88,7 +108,6 @@ public class FileChannelTest {
                 System.arraycopy(bytes, 0, toTemp, temp.length, bytes.length);
                 temp = toTemp;
             }
-            //buffer.flip();
             len = fileChannel.read(buffer);
         }
         fileChannel.close();
